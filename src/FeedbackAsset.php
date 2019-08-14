9fffe9438914e219841a74d107da997d80ee4a4b
andrey <6097033725>
2019-08-15 02:59:04 +0300

    misc

--- src/FeedbackAsset.php
+++ src/FeedbackAsset.php
@@ -8,13 +8,12 @@ use yii\web\AssetBundle;
  */
 class FeedbackAsset extends AssetBundle
 {
-	public $basePath = '@webroot';
-	public $baseUrl = '@web';
+	public $sourcePath = '@andrewdanilov/feedback/web';
 	public $css = [
 		'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css',
 	];
 	public $js = [
-		'/js/feedback.js',
+		'js/feedback.js',
 		'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js',
 	];
 	public $depends = [
